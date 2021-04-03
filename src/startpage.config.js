c471e89379012412b29e507b1a974876c32c2786
poesty <68623564+poesty@users.noreply.github.com>
2021-04-03 17:37:09 +0800

    Update startpage.config.js

--- src/startpage.config.js
+++ src/startpage.config.js
@@ -1,31 +1,21 @@
 let config = {
   terminal: {
-    title: `Terminal at i@186526.xyz`,
+    title: `Terminal at bleasky.cf`,
     user: 'i',
     hostname: '186526.xyz',
-    welcome: 'Welcome to <d color="#00f501">186526.xyz</d> terminal',
+    welcome: 'Welcome to <d color="#00f501">iam.bleasky.cf</d> terminal',
     actions: [],
   },
   friends: [
     {
-      name: 'Skrshadow',
-      description: '是红雨 昆卡昆卡斯哈斯哈',
-      url: 'https://blog.skrshadown.cn',
+      name: '1',
+      description: '1',
+      url: '1',
     },
     {
-      name: 'Radium-Bit',
-      description: '在B站py友链的朋友（',
-      url: 'https://radium-bit.github.io/',
-    },
-    {
-      name: `JimmyQin's Blog`,
-      description: '是大佬的说',
-      url: 'https://www.jimmyqin.cn/',
-    },
-    {
-      name: 'Lapis Apple的回收站',
-      description: '又是大佬的说',
-      url: 'https://laple.top/',
+      name: '2',
+      description: '2',
+      url: '2',
     },
   ],
   neofetch: [
@@ -39,34 +29,24 @@ let config = {
     },
     {
       name: 'Blog',
-      value: '<d href="https://blogging.186526.xyz">blogging.186526.xyz</d>',
+      value: '<d href="https://blog.bleasky.cf/">blog.bleasky.cf</d>',
     },
     {
       name: 'Telegram',
-      value: '<d href="https://t.me/real186526">@real186526</d>',
+      value: '<d href="https://t.me/bleasky">@bleasky</d>',
     },
     {
       name: 'Github',
-      value: '<d href="https://github.com/186526">@186526</d>',
+      value: '<d href="https://github.com/poesty">@poesty</d>',
     },
     {
       name: 'email',
-      value: '<d href="mailto:i@186526.xyz">i@186526.xyz</d>',
+      value: '<d href="mailto:poi@outlook.lv">poi@outlook.lv</d>',
     },
     {
-      name: 'Coolapk',
+      name: 'GPG',
       value:
-        '<d href="https://www.coolapk.com/u/848581.">https://www.coolapk.com/u/848581.</d>',
-    },
-    {
-      name: 'GPG [i@186526.xyz]',
-      value:
-        '<d href="https://i.186526.xyz/pgp_keys.asc">https://i.186526.xyz/pgp_keys.asc</d>',
-    },
-    {
-      name: 'S/MIME Key [i@186526.xyz]',
-      value:
-        '<d href="https://i.186526.xyz/i@186526.xyz.pem">https://i.186526.xyz/i@186526.xyz.pem</d>',
+        '<d href="https://keybase.io/poesty/pgp_keys.asc">https://keybase.io/poesty/pgp_keys.asc</d>',
     },
     {
       name: 'Shell',
